5e2d0bfc3ffc8b1039ac5b402e0a77af33012100
Samuel <samuel@techaffinity.com>
2023-12-24 11:58:12 +0200

    updates

--- utils/deleteFile.js
+++ utils/deleteFile.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const deleteFile = (fileName) => {
-  const filePath = path.join(__dirname, "../uploads", fileName); // A
+  // Get the root directory
+  const rootDirectory = path.join(__dirname, "..");
+  const filePath = path.join(rootDirectory, "uploads", fileName); // A
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       // Handle the error (file doesn't exist)
